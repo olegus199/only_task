c6e024bb58cb7d589af3979a2be4adb91ed128a1
olegus199 <okremnyov@yandex.ru>
2025-03-07 13:00:26 +0300

    init commit

---
+++ src/app/App.tsx
@@ -0,0 +1,13 @@
+import "./App.scss";
+import HistoricalDatesSection from "../common/HistoricalDatesSection.tsx";
+
+function App() {
+
+  return (
+    <>
+      <HistoricalDatesSection />
+    </>
+  );
+}
+
+export default App;
